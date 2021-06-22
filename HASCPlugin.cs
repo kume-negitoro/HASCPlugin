83613d2d08cacd12a3b6ac39617fa9e541747100
kume-negitoro <kume.negitoro@gmail.com>
2021-06-22 18:26:36 +0900

    setup project

--- HASCPlugin.cs
+++ HASCPlugin.cs
@@ -1,8 +1,50 @@
 using System;
 
+using Plugin;
+
 namespace HASCPlugin
 {
-    public class HASCPlugin
+    public class HASCPlugin : IPlugin
     {
+        public string Name
+        {
+            get
+            {
+                return "HASCPlugin";
+            }
+        }
+
+        public string Version
+        {
+            get
+            {
+                System.Reflection.Assembly asm = System.Reflection.Assembly.GetExecutingAssembly();
+                System.Version ver = asm.GetName().Version;
+                return ver.ToString();
+            }
+        }
+
+        public string Description
+        {
+            get
+            {
+                return "HASCプラグイン";
+            }
+        }
+
+        public IPluginHost Owner
+        {
+            get; set;
+        }
+
+        public void Run()
+        {
+
+        }
+
+        public void ExecuteTemplate(History history)
+        {
+
+        }
     }
 }
